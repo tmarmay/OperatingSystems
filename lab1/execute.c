b207f0ad1f9fae2c4257a6b8df70e2f360d74011
Tomas's Surface <tomasmarmay21@gmail.com>
2022-09-08 22:46:37 -0300

    propuesta para cambio de execute

--- lab1/execute.c
+++ lab1/execute.c
@@ -7,48 +7,78 @@
 #include "builtin.h"
 #include "strextra.h"
 
+static bool is_redirIn(scommand cmd){
+    return (scommand_get_redir_in(cmd) == NULL);
+}
+
+static bool is_redirOut(scommand cmd){
+    return (scommand_get_redir_out(cmd) == NULL);
+}
+
+static void free_mem(scommand cmd, char **argument_list, unsigned int length){
+    char *aux;
+    scommand_destroy(cmd);
+    for (unsigned int i = 0u; i < length + 1; i++){
+        aux = argument_list[i];
+        free(aux);
+    }
+    aux = NULL;
+    free(argument_list);
+    argument_list = NULL;
+}
+
+static char **exec_cmd_parsing(unsigned int length, scommand cmd){
+    char *aux;
+    char **argument_list = malloc(length * sizeof(char *));
+
+    //Parseo los argumentos del cmd en la forma que execvp() los necesita
+    for (unsigned int i = 0u; i < length; i++) {
+        aux = scommand_front(cmd);
+        argument_list[i] = malloc(strlen(aux));
+        scommand_pop_front(cmd);
+
+        // Agrega NULL como ultimo elemento del array
+        if (i == (length-1)) {
+            argument_list[i + 1u] = malloc(0);
+            argument_list[i+1] = NULL;
+        }
+    }
+    return argument_list;
+}
+
 static void execute_scommand(scommand cmd) {
     assert(cmd != NULL);
     char **argument_list;
     unsigned int length;
-    char *aux;
+    int fd[2]; 
 
     // Si es interno, usa las funciones de builtin para correr el comando
     if (builtin_is_internal(cmd)) {
         builtin_run(cmd);
 
     // Sino, usa execvp()
-    } else {
+    } else {       
+         
         length = scommand_length(cmd);
-        argument_list = malloc(length * sizeof(char *));
+        
+        argument_list = exec_cmd_parsing(length,cmd);
 
-        //Parseo los argumentos del cmd en la forma que execvp() los necesita
-        for (unsigned int i = 0u; i < length; i++) {
-            aux = scommand_front(cmd);
-            argument_list[i] = malloc(strlen(aux));
-            scommand_pop_front(cmd);
-
-
-            // Agrega NULL como ultimo elemento del array
-            if (i == (length-1)) {
-                argument_list[i + 1u] = malloc(0);
-                argument_list[i+1] = NULL;
-            }
-        }
-
+        /*
+        //aca tengo que ver si usa los redirecores o no
+        if() {
+         
+        //ver que hacer con toda esta parte
+        } else {
         // Ejecuta el comando
-        execvp(argument_list[0], argument_list);
+        if (execvp(argument_list[0], argument_list) == -1) { exit(EXIT_FAILURE) }; // o va un assert ?
+        }
+        */
 
         //Libera memoria
-        scommand_destroy(cmd);  
-        for (unsigned int i = 0u; i < length + 1; i++) {
-            aux = argument_list[i];
-            free(aux);
-        }
-        free(argument_list);
+        free_mem(cmd,argument_list,length);
     }
-
 }
+
 
 //al principio no iria un if viendo si es interno y llamando al builtin
 void execute_pipeline(pipeline apipe) {
@@ -57,6 +87,7 @@ void execute_pipeline(pipeline apipe) {
     scommand cmd;
     int rc_wait = 0;
     int rc = 0;
+    char *redir_in,redir_out;
 
     for (unsigned int i = 0u; i < length; i++){
         cmd = pipeline_front(apipe);
@@ -80,18 +111,19 @@ void execute_pipeline(pipeline apipe) {
 
 
         //-Caso en el que NO esperamos
+            // no esperar signifa hacerlo por background enotnces no esperar seria hacer lo mismo que arriba pero sin el wait 
         } else{
-            //+++COMPLETAR+++
+            rc = fork();
+            assert(!(rc < 0));
+            //Hijo
+            if (rc == 0){
+                execute_scommand(cmd); //no me ejecutaria un comando y despues se muere ? 
+            }
         }
-
-    pipeline_destroy(apipe);
+ 
+    }
+    if(!pipeline_is_empty(apipe)) {
+        pipeline_destroy(apipe);
     }
 }
 
-/*
- * Ejecuta un pipeline, identificando comandos internos, forkeando, y
- *   redirigiendo la entrada y salida. puede modificar `apipe' en el proceso
- *   de ejecución.
- *   apipe: pipeline a ejecutar
- * Requires: apipe!=NULL
- */
