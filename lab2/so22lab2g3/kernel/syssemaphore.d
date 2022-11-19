bf81b84470f8294238860f591bf9066bc36dd67f
Tomas's Home <tomasmarmay21@gmail.com>
2022-11-19 12:14:26 -0300

    lab2(semaforos) terminado

--- lab2/so22lab2g3/kernel/syssemaphore.d
+++
@@ -1,5 +0,0 @@
-kernel/syssemaphore.o: kernel/syssemaphore.c \
- /usr/lib/gcc-cross/riscv64-linux-gnu/9/include/stddef.h kernel/types.h \
- kernel/riscv.h kernel/defs.h kernel/param.h kernel/memlayout.h \
- kernel/spinlock.h kernel/proc.h \
- /usr/lib/gcc-cross/riscv64-linux-gnu/9/include/stdbool.h
--- lab2/so22lab2g3/user/test.d
+++
@@ -1,2 +0,0 @@
-user/test.o: user/test.c kernel/types.h kernel/stat.h user/user.h \
- /usr/lib/gcc-cross/riscv64-linux-gnu/9/include/stddef.h
